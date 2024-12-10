0fa0f07bea7bce0401233e6e5bde12bf80954b93
Joshua Soden <joshuasodenmedia@hotmail.com>
2024-12-10 21:08:31 +0000

    Create separate time container component

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,15 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
 
+import TimeContainer from "./components/TimeContainer/TimeContainer";
+
 function App() {
 
-  const [serverTime, setServerTime] = useState(null);
   const [serverMetrics, setServerMetrics] = useState([]);
-  const [serverError, setServerError] = useState(false);
-  const [timeLoading, setTimeLoading] = useState(false);
   const [metricsLoading, setMetricsLoading] = useState(false);
-  const [timer, setTimer] = useState(0);
   const timerMs = 30000;
 
   useEffect(() => {
-    function retrieveServerTime() {
-      setTimeLoading(true);
-      fetch('http://localhost:5001/time/', {headers: {authorization: process.env.REACT_APP_ACCESS_TOKEN}})
-      .then(response => response.text())
-      .then((data) => {
-        let epochTime = JSON.parse(data).epoch;
-        setServerTime(epochTime);
-        setTimer(0);
-        setServerError(false);
-      })
-      .catch(err => {
-        console.log("Error retrieving data");
-        setServerError(true);
-      })
-      .finally(() => {
-        console.log("Loading complete!");
-        setTimeLoading(false);
-      });
-    }
 
     function retrieveServerMetrics() {
       setMetricsLoading(true);
@@ -53,7 +32,7 @@ function App() {
     }
 
     function retrieveServerMetricsAndTime() {
-      retrieveServerTime();
+      // retrieveServerTime();
       retrieveServerMetrics();
     }
 
@@ -66,22 +45,7 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
-  // For the timer
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimer(prevTimer => prevTimer + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const formatTime = (seconds) => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-  
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
-  }
+ 
 
   const convertMetricsToArray = (metrics) => {
     return metrics.split("\n");
@@ -104,16 +68,7 @@ function App() {
       <main>
         <section className="light">
           <div className="content">
-            <h2>Server Information</h2>
-            <div>
-              {timeLoading ? (
-                <p>Server time: Loading...</p>
-              ) : (
-                <p>Server time: {serverTime}</p>
-              )} 
-              {serverError && <p>Error retrieving data. Trying in 30s...</p>}
-              <p class="large">{formatTime(timer)}</p>
-            </div>
+            <TimeContainer />
           </div>
         </section>
 
